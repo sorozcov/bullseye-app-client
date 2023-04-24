8ea72ccdfe98303301f9a8bd825e6b6e4ffa5652
sorozcov <silvio1299@gmail.com>
2023-04-24 14:25:26 -0600

    Get Locations without filters

--- src/components/LocationsList/index.js
+++ src/components/LocationsList/index.js
@@ -1,16 +1,48 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Col, Container, Row, Spinner } from "react-bootstrap";
+import { BASE_API_URL,API_KEY, CLIENT_ID } from "../../config";
+import axios from "axios";
 
 export default function LocationsList(){
+
+    // States to manage locations and loading
+    const [locations,setLocations] = useState([]);
+    const [areLocationsLoading,setAreLocationLoading] = useState(false);
+
+    // TODO Optimize search as it can be paginated
+    // Function to refresh locations from API
+    const refreshLocations = async ()=>{
+        try{
+            setAreLocationLoading(true);
+            const params = {ClientId:CLIENT_ID,ApiKey:API_KEY,StartIndex:0,PageSize:50,CountryID:1};
+            const response = await axios.get(`${BASE_API_URL}/restsearch.svc/dosearch2`,{params});
+            if(response.status===200){
+                setLocations(response.data.ResultList);
+            }else{
+                alert("Error loading locations")
+            }
+            setAreLocationLoading(false);
+        }catch(e){
+            setAreLocationLoading(false);
+            console.log(e);
+        }
+    };
+
+    // Get locations the first time on build
+    useEffect(()=>{
+        refreshLocations();
+    },[])
+
     return (
-    <Container style={{marginTop:'2vh'}}>
-        <Row>
+    <div style={{marginTop:'3vh',marginLeft:'5vw',marginRight:'2vw',marginBottom:'5vh'}}>
+        {areLocationsLoading ? <Spinner/> : <><Row>
             <Col sm={12} md={4}>
-                Here goes the list
+                {locations.map(loc=>loc.Address1)}
             </Col>
             <Col sm={12} md={8}>
                 Here goes the map
             </Col>
-        </Row>
-    </Container>
+            </Row></>}
+    </div>
     )
 }
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const BASE_API_URL = "https://ws.bullseyelocations-staging.com";
+export const CLIENT_ID = 7205;
+export const API_KEY = "636932b6-09b2-41fb-af64-701c9d758821-zxyab";
